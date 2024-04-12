a5c2b9f2cc7fbf8edf310edb8ff59aad06365aab
huyga154 <87904983+huyga154@users.noreply.github.com>
2024-04-12 13:34:10 +0700

    Add files via upload

--- MOOC3/W2/js/gallery.js
+++ MOOC3/W2/js/gallery.js
@@ -14,7 +14,7 @@ function upDate(previewPic){
        
     }
    
-       function unDo(){
+function unDo(){
         /* In this function you should 
        1) Update the url for the background image of the div with the id = "image" 
        back to the orginal-image.  You can use the css code to see what that original URL was
@@ -22,5 +22,8 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        back to the original text.  You can use the html code to see what that original text was
        */
+       var elm = document.getElementById("image"); // Pass the ID as a string
+       elm.style.backgroundImage = '';
+       elm.innerText = 'Hover over an image below to display here.';
            
        }
